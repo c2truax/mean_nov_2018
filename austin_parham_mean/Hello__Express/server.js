9f283f77a14e89061fe6f4dddcee50327dc631d8
austinmparham <42558200+austinmparham@users.noreply.github.com>
2018-11-13 16:29:53 -0600

    End of Week 2

--- austin_parham_mean/Hello__Express/server.js
+++ austin_parham_mean/Hello__Express/server.js
@@ -4,6 +4,11 @@ console.log("Let's find out what express is", express);
 // invoke express and store the result in the variable app
 var app = express();
 console.log("Let's find out what app is", app);
+
+app.use(express.static( __dirname + '/public/dist/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
 // use app's get method and pass it the base route '/' and a callback
 app.get('/', function(request, response) {
     // just for fun, take a look at the request and response objects
@@ -35,10 +40,6 @@ app.get("/form", function (request,response){
   response.render('form');
 });
 
-
-app.use(express.static(__dirname + "/static"));
-app.set('views', __dirname + '/views');
-app.set('view engine','ejs');
 
 
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
